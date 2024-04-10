bec77a0c8d07677e658871a04d1de32b9247797c
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-04-10 19:17:11 -0400

    instructions.dart 1.0;

--- lib/instructions.dart
+++ lib/instructions.dart
@@ -1,84 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:colorme/takeSelfie.dart';
 
-
-List<String> images = [
-  "assets/Instruction01.png",
-  "assets/Instruction02.png",
-  "assets/Instruction03.png",
-];
-
-class Test extends StatefulWidget {
-  const Test({Key? key}) : super(key: key);
+class Instructions extends StatefulWidget {
+  Instructions({Key? key}) : super(key: key);
 
   @override
-  State<Test> createState() => _TestState();
+  State<Instructions> createState() => _InstructionsState();
 }
 
-class _TestState extends State<Test> {
-  @override
-  Widget build(BuildContext context) {
-    return Image.asset("assets/Instruction01.png");
-  }
-}
+class _InstructionsState extends State<Instructions> {
+  int _currentIndex = 0;
 
-
-
-class Instructions extends StatelessWidget {
-  const Instructions({Key? key}) : super(key: key);
-
+  final List<String> imagePaths = [
+    "assets/Instruction01.png",
+    "assets/Instruction02.png",
+    "assets/Instruction03.png"
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          Row(
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(height: 100),
+            Text(
+              "colorme.",
+              style: TextStyle(
+                fontSize: 40,
+                fontFamily: 'LeagueSpartan',
+                fontWeight: FontWeight.w800,
+              ),
+            ),
+            SizedBox(height: 35),
+            Text(
+              "Instructions",
+              style: TextStyle(
+                fontSize: 37,
+                fontFamily: 'LeagueSpartan',
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            SizedBox(height: 20),
+            CarouselSlider.builder(
+              itemCount: imagePaths.length,
+              itemBuilder: (BuildContext context, int index, int realIndex) {
+                return Image.asset(
+                  imagePaths[index],
+                  fit: BoxFit.cover,
+                );
+              },
+              options: CarouselOptions(
+                height: 300,
+                enableInfiniteScroll: true,
+                viewportFraction: 1.0,
+                enlargeCenterPage: false,
+                autoPlay: true,
+                autoPlayInterval: Duration(seconds: 2),
+                autoPlayAnimationDuration: Duration(milliseconds: 800),
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _currentIndex = index;
+                  });
+                },
+              ),
+            ),
+            SizedBox(height: 20),
+            Row(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(
-                      top: 100.0
+              children: imagePaths.map((url) {
+                int index = imagePaths.indexOf(url);
+                return Container(
+                  width: 12.0,
+                  height: 12.0,
+                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: _currentIndex == index ? Colors.black : Colors.grey.shade400,
                   ),
-                  child: const Text(
-                    "colorme.",
-                    style: TextStyle(
-                      fontSize: 40,
-                      fontFamily: 'LeagueSpartan',
-                      fontWeight: FontWeight.w800,
+                );
+              }).toList(),
+            ),
+            SizedBox(height: 25),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => TakeSelfie()),
+                );
+              },
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12.0)
+                  ),
+                  backgroundColor: Colors.deepOrange
+              ),
+              child: Text('Take Selfie'),
+            ),
+            Expanded(
+              child: Stack(
+                alignment: Alignment.bottomRight,
+                children: [
+                  Align(
+                    alignment: Alignment.bottomRight,
+                    child: Positioned.fill(
+                      child: Image.asset(
+                        "assets/Instruction.png",
+
+                      ),
                     ),
-                  ),
-                )]
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  margin: const EdgeInsets.only(
-                      top: 35.0
-                  ),
-                  child: const Text(
-                    "Instructions",
-                    style: TextStyle(
-                      fontSize: 37,
-                      fontFamily: 'LeagueSpartan',
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                )]
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                child: Test(),
-              )
-            ],
-          )
-        ],
+                  )
+
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
-
-
   }
+}
+
+void main() {
+  runApp(Instructions());
 }
---
+++ lib/takeSelfie.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+
+class TakeSelfie extends StatefulWidget {
+  const TakeSelfie({Key? key}) : super(key: key);
+
+  @override
+  State<TakeSelfie> createState() => _TakeSelfieState();
+}
+
+class _TakeSelfieState extends State<TakeSelfie> {
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Take Selfie Page');
+  }
+}
+
+
