966820ef52e22733c321bbe115b42549fb282161
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-04-10 13:14:05 -0400

    Changes to instructions.dart

--- lib/instructions.dart
+++ lib/instructions.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
+
+List<String> images = [
+  "assets/Instruction01.png",
+  "assets/Instruction02.png",
+  "assets/Instruction03.png",
+];
+
+class Test extends StatefulWidget {
+  const Test({Key? key}) : super(key: key);
+
+  @override
+  State<Test> createState() => _TestState();
+}
+
+class _TestState extends State<Test> {
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset("assets/Instruction01.png");
+  }
+}
+
+
+
 class Instructions extends StatelessWidget {
   const Instructions({Key? key}) : super(key: key);
+
 
   @override
   Widget build(BuildContext context) {
@@ -43,8 +67,18 @@ class Instructions extends StatelessWidget {
                   ),
                 )]
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                child: Test(),
+              )
+            ],
+          )
         ],
       ),
     );
+
+
   }
 }
